e0751fb4a060e351c847b4daffdd5322e576e6f2
seiichi1101 <seiichi19881101@gmail.com>
2024-06-18 10:52:42 +0200

    Fix management of history file

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ This is a tool to generate a link of CloudFormation template for switching role.
 
 ## Example
 
-- Admin Switch Role Stack: <https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/review?&templateURL=https://arai-switch-role-temlate-buckt.s3.us-east-1.amazonaws.com/admin-switch-role.yaml&stackName=admin-switch-role-stack&param_XXXBucketURL=https://arai-switch-role-temlate-buckt.s3.us-east-1.amazonaws.com>
-- Viewer Switch Role Stack: <https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/review?&templateURL=https://arai-switch-role-temlate-buckt.s3.us-east-1.amazonaws.com/viewer-switch-role.yaml&stackName=viewer-switch-role-stack&param_XXXBucketURL=https://arai-switch-role-temlate-buckt.s3.us-east-1.amazonaws.com>
+- Admin Switch Role Stack: <https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/review?&templateURL=https://arai-switch-role-template-bucket.s3.us-east-1.amazonaws.com/admin-switch-role.yaml&stackName=admin-switch-role-stack&param_XXXBucketURL=https://arai-switch-role-template-bucket.s3.us-east-1.amazonaws.com>
+- Viewer Switch Role Stack: <https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/review?&templateURL=https://arai-switch-role-template-bucket.s3.us-east-1.amazonaws.com/viewer-switch-role.yaml&stackName=viewer-switch-role-stack&param_XXXBucketURL=https://arai-switch-role-template-bucket.s3.us-east-1.amazonaws.com>
 
 ## Pre-requirements
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,40 +1,46 @@
-module Lib
+    module Lib
   ( cmdFunc,
   )
 where
 
-import Configuration.Dotenv (defaultConfig, loadFile)
+import Configuration.Dotenv (defaultConfig, loadFile, onMissingFile)
 import System.Environment (lookupEnv)
 import System.IO
-import System.Directory (doesFileExist)
+import System.Directory (doesFileExist, doesDirectoryExist, createDirectory)
 import Data.Maybe (fromMaybe)
-
-
+import System.Environment (getExecutablePath)
+import System.FilePath (takeDirectory)
 
 switchRoleLink :: String
 switchRoleLink =
   "https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/review?"
 
-readPreviousInput :: FilePath -> IO (Maybe String)
-readPreviousInput filePath = do
-  fileExists <- doesFileExist filePath
+readPreviousInput :: String -> String -> IO (Maybe String)
+readPreviousInput fileDir fileName = do
+  fileExists <- doesFileExist (fileDir ++ "/" ++ fileName)
   if fileExists
-    then Just <$> readFile filePath
+    then Just <$> readFile (fileDir ++ "/" ++ fileName)
     else return Nothing
 
-writeInput :: FilePath -> String -> IO ()
-writeInput filePath input = writeFile filePath input
+writeInput :: String -> String -> String -> IO ()
+writeInput fileDir fileName input = do
+  dirExists <- doesDirectoryExist fileDir
+  if not dirExists
+    then createDirectory fileDir
+    else return ()
+  writeFile (fileDir ++ "/" ++ fileName) input
+  
 
-promptWithHistory :: String -> FilePath -> IO String
-promptWithHistory prompt filePath = do
-  previousInput <- readPreviousInput filePath
+promptWithHistory :: String -> String -> String -> IO String
+promptWithHistory prompt fileDir fileName = do
+  previousInput <- readPreviousInput fileDir fileName
   let defaultMessage = maybe "" (\prev -> " (press enter to use \"" ++ prev ++ "\")") previousInput
   putStr $ prompt ++ defaultMessage ++ ": "
   hFlush stdout
   input <- getLine
   if null input
     then return $ fromMaybe "" previousInput
-    else writeInput filePath input >> return input
+    else writeInput fileDir fileName input >> return input
 
 addQueryParams :: String -> [(String, String)] -> String
 addQueryParams url [] = url
@@ -44,16 +50,25 @@ addQueryParams url ((key, value) : rest)
 
 cmdFunc :: IO ()
 cmdFunc = do
-  loadFile defaultConfig
-  bucket <- lookupEnv "TEMPLATE_STORING_BUCKET"
-  let bucketURL = case bucket of
-        Just b -> "https://" ++ b ++ ".s3.us-east-1.amazonaws.com"
-        Nothing -> error "TEMPLATE_STORING_BUCKET is not set in .env file."
-  let baseStackName = "switch-role-stack"
-  sourceAwsAccountId <- promptWithHistory "Enter Source AWS Account ID" "history/source-aws-account-id.txt"
-  sourceIamUserName <- promptWithHistory "Enter Source IAM User Name" "history/source-iam-user-name.txt"
-  targetAwsAccountId <- promptWithHistory "Enter Target AWS Account ID" "history/target-aws-account-id.txt"
+  -- get dir path
+  executablePath <- getExecutablePath
+  let execDir = takeDirectory executablePath
+  let dirPath = execDir ++ "/history"
 
+  -- load .env
+  onMissingFile (loadFile defaultConfig) (return ())
+  bucket <- lookupEnv "TEMPLATE_STORING_BUCKET"
+  case bucket of
+        Just b -> writeInput dirPath "template-storing-bucket.txt" b
+        Nothing -> return ()
+
+  -- prompt for inputs
+  templateStoringBucketName <- promptWithHistory "Enter Template Storing Bucket Name" dirPath "template-storing-bucket.txt"
+  let templateBucketURL = "https://" ++ templateStoringBucketName ++ ".s3.us-east-1.amazonaws.com"
+  sourceAwsAccountId <- promptWithHistory "Enter Source AWS Account ID" dirPath "source-aws-account-id.txt"
+  sourceIamUserName <- promptWithHistory "Enter Source IAM User Name" dirPath "source-iam-user-name.txt"
+  targetAwsAccountId <- promptWithHistory "Enter Target AWS Account ID" dirPath "target-aws-account-id.txt"
+  let baseStackName = "switch-role-stack"
   -- show all inputs
   putStrLn "============================="
   putStrLn "The following parameters will be used:"
@@ -65,8 +80,8 @@ cmdFunc = do
   let adminStackName = "admin-" ++ baseStackName ++ "-for-" ++ sourceAwsAccountId ++ "-" ++ sourceIamUserName
   let viewerStackName = "viewer-" ++ baseStackName ++ "-for-" ++ sourceAwsAccountId ++ "-" ++ sourceIamUserName
 
-  let adminStackLink = addQueryParams switchRoleLink [("templateURL", bucketURL ++ "/admin-switch-role.yaml"), ("stackName", adminStackName), ("param_SourceAWSAccountId", sourceAwsAccountId), ("param_SourceIAMUserName", sourceIamUserName), ("param_XXXTargetAWSAccountId", targetAwsAccountId), ("param_XXXBucketURL", bucketURL)]
-  let viewerStackLink = addQueryParams switchRoleLink [("templateURL", bucketURL ++ "/viewer-switch-role.yaml"), ("stackName", viewerStackName), ("param_SourceAWSAccountId", sourceAwsAccountId), ("param_SourceIAMUserName", sourceIamUserName), ("param_XXXTargetAWSAccountId", targetAwsAccountId), ("param_XXXBucketURL", bucketURL)]
+  let adminStackLink = addQueryParams switchRoleLink [("templateURL", templateBucketURL ++ "/admin-switch-role.yaml"), ("stackName", adminStackName), ("param_SourceAWSAccountId", sourceAwsAccountId), ("param_SourceIAMUserName", sourceIamUserName), ("param_XXXTargetAWSAccountId", targetAwsAccountId), ("param_XXXBucketURL", templateBucketURL)]
+  let viewerStackLink = addQueryParams switchRoleLink [("templateURL", templateBucketURL ++ "/viewer-switch-role.yaml"), ("stackName", viewerStackName), ("param_SourceAWSAccountId", sourceAwsAccountId), ("param_SourceIAMUserName", sourceIamUserName), ("param_XXXTargetAWSAccountId", targetAwsAccountId), ("param_XXXBucketURL", templateBucketURL)]
 
   -- show links
   putStrLn "============================="
